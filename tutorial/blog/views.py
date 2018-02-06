a2c29adc27748fd703a81bfa89b6219c0fe5cc02
Junhong-Kim <jhk1005jhk@gmail.com>
2018-02-06 15:08:18 +0900

    Added, get_django & set_modified

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
+
 from .models import Post
 from .serializers import PostSerializer
 
@@ -7,6 +10,8 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    # HTTP GET /blog/post/
+    # HTTP GET /blog/post/?search=
     def get_queryset(self):
         qs = super().get_queryset()
 
@@ -15,3 +20,19 @@ class PostViewSet(viewsets.ModelViewSet):
             qs = qs.filter(message__icontains=search)
 
         return qs
+
+    # HTTP GET /blog/post/get_django/
+    @list_route()
+    def get_django(self, request):
+        qs = self.get_queryset().filter(message__icontains='django')
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
+
+    # HTTP PATCH /blog/post/{pk}/django
+    @detail_route(methods=['patch'])
+    def set_modified(self, request, pk):
+        instance = self.get_object()
+        instance.message = instance.message + '(modified)'
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
