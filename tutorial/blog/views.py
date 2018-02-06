0d86604af0d5b237dcd4ce9b39e6955d8edfe09e
Junhong-Kim <jhk1005jhk@gmail.com>
2018-02-06 12:28:40 +0900

    Added, search for Post

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -6,3 +6,12 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        search = self.request.query_params.get('search', '')
+        if search:
+            qs = qs.filter(message__icontains=search)
+
+        return qs
