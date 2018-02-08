13cd3193ca342391df81dbeb4791fb9fe74b0a09
Junhong-Kim <jhk1005jhk@gmail.com>
2018-02-08 12:05:59 +0900

    Added, FBV of Comment

--- tutorial/tutorial/settings/base.py
+++ tutorial/tutorial/settings/base.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'blog',
     'post',
+    'comment',
 ]
 
 REST_FRAMEWORK = {
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('post/', include('post.urls')),
+    path('comment/', include('comment.urls')),
 ]
